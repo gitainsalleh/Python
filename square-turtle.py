0e10105983be367285268c98ec1089e3eb1a0fe1
Ain Salleh <code.ainsalleh@gmail.com>
2023-04-11 11:32:10 +0800

    Drawn a square

---
+++ square-turtle.py
@@ -0,0 +1,19 @@
+#Drawing a turtle with package Turtle
+
+import turtle
+
+turtle.bgcolor("white") # Background color
+turtle.pensize(2) # Pen Size
+turtle.color("green") # Turtle color 
+
+# Draw a square
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.done() #Allowing turtle to stay on screen
